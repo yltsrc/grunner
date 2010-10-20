fc6a40baa55fd1ff46f41a917af98a567058ac41
Yura Tolstik <yltsrc@gmail.com>
2010-10-20 14:27:35 +0300

    read applications from $HOME applications config

--- main.c
+++ main.c
@@ -3,30 +3,61 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <pwd.h>
 
-#define CMDLEN 256
+#define CMDLEN NAME_MAX+1
 #define DIRECTORY "/usr/share/applications/"
 #define NAME "Name="
 #define HIDDEN "NoDisplay=true"
 #define EXEC "Exec="
 
-int main(void)
-{
-    char *fn = (char *)malloc(CMDLEN+strlen(DIRECTORY));
+void concat_applications(DIR *mydir, char *dir, FILE *finput, int fdin, int fdout) {
+
+    char *fn = (char *)malloc(CMDLEN+strlen(dir));
     char *buffer = (char *)malloc(CMDLEN);
     char *exec = (char *)malloc(CMDLEN);
-    char *cmd = (char *)malloc(CMDLEN);
     gboolean hidden;
-    DIR *mydir = opendir(DIRECTORY);
     FILE *fp;
-    FILE *finput;
-    FILE *foutput;
+    struct dirent *entry = NULL;
+
+    while((entry = readdir(mydir)))
+    {
+        hidden = FALSE;
+
+        strcpy(fn, dir);
+        strcat(fn, entry->d_name);
+
+        if((fp = fopen(fn, "r")) != NULL) {
+            while( fgets(buffer, CMDLEN, fp) != NULL ) {
+                if (!strncmp(buffer, HIDDEN, strlen(HIDDEN))) {
+                    hidden = TRUE;
+                }
+
+                if (!strncmp(buffer, EXEC, strlen(EXEC))) {
+                    strcpy(exec, buffer+strlen(EXEC));
+                }
+            }
+        }
+
+        if (!hidden) {
+            fprintf(finput, "%s", exec);
+        }
+
+        fclose(fp);
+
+    }
+}
+
+int main(void)
+{
+    char *cmd = (char *)malloc(CMDLEN);
     int fdout;
     int fdin;
     GPid child_pid;
+    FILE *finput;
+    FILE *foutput;
     char *argv[] = {"/bin/sh", "-c", "gpicker --name-separator=\"\\n\" -", 0};
-    struct dirent *entry = NULL;
-
     gboolean ok = g_spawn_async_with_pipes(0, // work dir
                                            argv,
                                            0, //envp
@@ -47,34 +78,19 @@ int main(void)
             return 2;
         }
 
-        while((entry = readdir(mydir)))
-        {
-            hidden = FALSE;
-
-            strcpy(fn, DIRECTORY);
-            strcat(fn, entry->d_name);
-
-            if((fp = fopen(fn, "r")) != NULL) {
-                while( fgets(buffer, CMDLEN, fp) != NULL ) {
-                    if (!strncmp(buffer, HIDDEN, strlen(HIDDEN))) {
-                        hidden = TRUE;
-                    }
-
-                    if (!strncmp(buffer, EXEC, strlen(EXEC))) {
-                        strcpy(exec, buffer+strlen(EXEC));
-                    }
-                }
-            }
-
-            if (!hidden) {
-                fprintf(finput, "%s", exec);
-            }
-
-            fclose(fp);
-
-        }
-
+        DIR *mydir = opendir(DIRECTORY);
+        concat_applications(mydir, DIRECTORY, finput, fdin, fdout);
         closedir(mydir);
+
+        uid_t id = getuid();
+        struct passwd *pw = getpwuid(id);
+        char *hd = (char *)malloc(CMDLEN);
+        strcpy(hd, pw->pw_dir);
+        strcat(hd, "/.local/share/applications/");
+
+        DIR *homedir = opendir(hd);
+        concat_applications(homedir, hd, finput, fdin, fdout);
+        closedir(homedir);
 
         fclose(finput);
 
